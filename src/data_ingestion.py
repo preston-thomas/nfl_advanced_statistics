4d62afa35c21d000d00b84d65139627b1e30f742
Preston Thomas <140858770+preston-thomas@users.noreply.github.com>
2024-11-13 23:01:58 -0500

    Fixed SQL connection string

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -7,6 +7,6 @@ import pandas as pd
 # Might have to modify depending on your local database configuration
 # Password will likely be different on your own installation
 connection_string = "dbname = nfl_data user = postgres password = nfl_football_rocks host = localhost port = 5432"
-conn = sql.connect("dbname = nfl_data user = postgres password = nfl_football_rocks host = localhost port = 5432")
+conn = sql.connect(connection_string)
 
 #TODO: Run various queries to extract raw data into data frames & transform
