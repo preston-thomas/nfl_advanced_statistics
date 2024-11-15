b24ba7dc879d01c046de1ae5315697fead3b2eee
Preston Thomas <140858770+preston-thomas@users.noreply.github.com>
2024-11-14 23:12:04 -0500

    Ingest all tables in the database
    Cleaning will come next.

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -7,8 +7,25 @@ import pandas as pd
 # Might have to modify depending on your local database configuration
 # Password will likely be different on your own installation
 engine = sql.create_engine("postgresql+psycopg2://postgres:nfl_football_rocks@localhost:5432/nfl_data")
-test_query = "SELECT season, draft_year, draft_team, draft_round FROM nfl_data.combine_data ORDER BY shuttle LIMIT 10"
+combine_query = "SELECT * FROM nfl_data.combine_data"
+player_query = "SELECT * FROM nfl_data.nfl_players"
+qbr_szn_query = "SELECT * FROM nfl_data.qbr_szn"
+qbr_week_query = "SELECT * FROM nfl_data.qbr_week"
+roster_query = "SELECT * FROM nfl_data.rosters_2024"
 
 if __name__ == "__main__":
-    test_df = pd.read_sql_query(test_query, engine)
-    print(test_df.head())
+    combine_raw = pd.read_sql_query(combine_query, engine)
+    player_raw = pd.read_sql_query(player_query, engine)
+    qbr_szn_raw = pd.read_sql_query(qbr_szn_query, engine)
+    qbr_week_raw = pd.read_sql_query(qbr_week_query, engine)
+    rosters_raw = pd.read_sql_query(roster_query, engine)
+    print("Null values in combine_raw:")
+    print(combine_raw.isnull().sum())
+    print("\nNull values in player_raw:")
+    print(player_raw.isnull().sum())
+    print("\nNull values in qbr_szn_raw:")
+    print(qbr_szn_raw.isnull().sum())
+    print("\nNull values in qbr_week_raw:")
+    print(qbr_week_raw.isnull().sum())
+    print("\nNull values in rosters_raw:")
+    print(rosters_raw.isnull().sum())
