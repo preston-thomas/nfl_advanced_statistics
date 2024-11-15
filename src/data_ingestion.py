08389c273b478899526b806d7211fafa5bdae4f6
Preston Thomas <140858770+preston-thomas@users.noreply.github.com>
2024-11-14 22:08:59 -0500

    Switching database module, prelim testing for loading SQL into data frame

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,18 +1,14 @@
 # All code for this program directly contributed by Preston Thomas
 
-import psycopg2 as sql
+import sqlalchemy as sql
 import pandas as pd
 
 # Establish database connection
 # Might have to modify depending on your local database configuration
 # Password will likely be different on your own installation
-connection_string = "dbname = nfl_data user = postgres password = nfl_football_rocks host = localhost port = 5432"
-conn = sql.connect(connection_string)
-cur = conn.cursor()
+engine = sql.create_engine("postgresql+psycopg2://postgres:nfl_football_rocks@localhost:5432/nfl_data")
+test_query = "SELECT season, draft_year, draft_team, draft_round FROM nfl_data.combine_data ORDER BY shuttle LIMIT 10"
 
-# Sample query and print statement to verify successful database connection made
-cur.execute("SELECT * FROM nfl_data.combine_data ORDER BY shuttle LIMIT 10")
-results = cur.fetchall()
-
-for row in results:
-    print(row)
+if __name__ == "__main__":
+    test_df = pd.read_sql_query(test_query, engine)
+    print(test_df.head())
