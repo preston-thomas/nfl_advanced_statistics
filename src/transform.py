6c57a804e11e0a8794ccfde818913a919c098e79
Preston Thomas <140858770+preston-thomas@users.noreply.github.com>
2024-12-04 17:04:28 -0500

    Added TODO comments to project
    School has been crazy... hoping to get some time to devote to this over winter break.

--- src/transform.py
+++ src/transform.py
@@ -4,3 +4,6 @@ import store_data as sd
 if __name__ == "__main__":
     for obj in sd.datasets:
         print(obj.get_query())
+
+#TODO: I think I want to narrow the scope of this project, comparing QBR to fantasy football performance, and then using QBR against matchups to try and predict the strongest fantasy football starts (standard full-PPR scoring)
+#Will try to consider rushing statistics, as well.
