cd09e33fe6c763d50080c9265418931603bf61c2
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2024-12-06 10:21:07 -0500

    Documentation for next steps

--- src/transform.py
+++ src/transform.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
 
 #TODO: I think I want to narrow the scope of this project, comparing QBR to fantasy football performance, and then using QBR against matchups to try and predict the strongest fantasy football starts (standard full-PPR scoring)
 #Will try to consider rushing statistics, as well.
+#Need to find open source/public fantasy football data, otherwise, I'll need to calculate it myself
+#Full PPR scoring: .04 points per passing yards, .1 per rush yard, 4 point passing TDs, 6 point rush/receiving
