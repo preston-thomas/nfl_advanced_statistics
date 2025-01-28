d10ca70015a5469816832bb261067e549aeb3592
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2025-01-27 21:00:31 -0500

    Adding llm_integration file, more coming here soon

---
+++ src/llm_integration.py
@@ -0,0 +1,7 @@
+from ollama import Ollama
+
+# Initialize client
+client = Ollama()
+
+# Define a prompt. This should be invoked in the main screen of the program, and should allow user queries to be entered
+prompt = """This is a test prompt, please respond with hello and tell me about yourself briefly."""
