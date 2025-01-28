2ee15d94bc8735ef310df1ed6054a99ad1c6101c
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2025-01-28 10:06:28 -0500

    Added comment for additional query logic for offensive line performance

--- src/transform.py
+++ src/transform.py
@@ -5,10 +5,10 @@ import data_ingestion as di
 # This query attempts to evaluate which teams have the best offensive lines
 # Logic: sum the average rush yards before contact, group by each team, order by the highest total
 # Short comings: fails to consider running back skill
-# TODO: Filter on non-playoff games (do not consider playoffs in the calculation)
-# TODO: Deepseek LLM integration via local install seems possible, and would add breadth to this project. Investigate how possible this is, see if it can query data to answer user prompts
 
 # Processing offensive line performance query
+# NOTE: This does not consider scheme in yards before contact
+# A team who runs more inside zone likely has more yards after contact and less before than a team that runs more outside zone
 def process_o_line_query() -> pd.DataFrame:
     o_line = di.Dataset()
     o_line.set_query("select SUM(rushing_yards_before_contact_avg) as szn_rush_yds_before_contact_avg_sum, MAX(team) as team, sum(carries) as szn_carries from nfl_data.ADVSTATS_WEEK_RUSH where season = 2024 and week <= 17 group by team order by SUM(rushing_yards_before_contact_avg) desc")
